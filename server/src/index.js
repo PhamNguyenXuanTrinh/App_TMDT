8eb93d5bd9ab38906b2dbbac207ff0d7931c16a9
Trinh Pham <pnxtrinh@gmail.com>
2023-12-03 14:14:34 +0700

    first code

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const dotenv = require ('dotenv')
+
+
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT)
